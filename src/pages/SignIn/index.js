1cfdadf2fb961f9b9782b8c4686d39ce3f483b9f
Gabrielncosta <gabrielncosta208@gmail.com>
2020-03-31 23:58:47 -0300

    Unform

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~assets/logo.svg';
-// import { Container } from './styles';
 
 export default function SignIn() {
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha" />
+      <Form>
+        <Input type="email" placeholder="Seu e-mail" />
+        <Input type="password" placeholder="Sua senha" />
         <button type="submit">Acessar</button>
-      </form>
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
     </>
   );
 }
