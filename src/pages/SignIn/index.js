498d383f9fced01e2628f494495c9749f7a26a73
Gabrielncosta <gabrielncosta208@gmail.com>
2020-03-31 22:58:19 -0300

    SignInPage

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import logo from '~assets/logo.svg';
+// import { Container } from './styles';
+
+export default function SignIn() {
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha" />
+        <button type="submit">Acessar</button>
+      </form>
+    </>
+  );
+}
