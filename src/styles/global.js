d1ba4bf45226f1d0c445e19c0d72751a35088a78
Gabrielncosta <gabrielncosta208@gmail.com>
2020-04-21 14:18:29 -0300

    dateFormatted

--- src/styles/global.js
+++ src/styles/global.js
@@ -3,8 +3,11 @@ import 'react-toastify/dist/ReactToastify.css';
 import 'react-perfect-scrollbar/dist/css/styles.css';
 
 export default createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
-
+  
+  @font-face {
+    font-family: Roboto, 'Roboto';
+    src: url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+  }
   * {
     margin: 0;
     padding: 0;
